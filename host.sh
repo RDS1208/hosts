9936e98e207654c7aabd89c952acc9234e023459
RDS1208 <davidrosu1208@gmail.com>
2025-12-10 13:05:43 +0200

    Modificare script pentru a primii argumentele din linia de comanda

--- host.sh
+++ host.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cat /etc/hosts | while read ip name rest; do
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && [[ -n "$name" ]]; then
-        ip_nslookup=$(nslookup "$name" 2>/dev/null | grep -A 1 'Name:' | grep 'Address:' | awk '{print $2}' | tail -n 1)
-        if [[ -n "$ip_nslookup" ]] && [[ "$ip" != "$ip_nslookup" ]]; then
-            echo "Bogus IP for $name in /etc/hosts! Config: $ip, Resolved: $ip_nslookup"
+nume_host = $2
+ip_arg = $3
+        ip_nslookup=$(nslookup "$nume_host" 2>/dev/null | grep -A 1 'Name:' | grep 'Address:' | awk '{print $2}' | tail -n 1)
+        if [[ -n "$ip_nslookup" ]] && [[ "$ip_arg" != "$ip_nslookup" ]]; then
+            echo "Bogus IP for $nume_host in /etc/hosts! Config: $ip_arg, Resolved: $ip_nslookup"
         fi
-    fi
-done
