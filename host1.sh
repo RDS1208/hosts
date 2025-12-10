848061cbf9cc5d0b15ffa07908fbe28b4cecbe75
RDS1208 <168984816+RDS1208@users.noreply.github.com>
2025-12-10 13:29:37 +0200

    Delete host1.sh

--- host1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-nume_host = $2
-ip_arg = $3
-        ip_nslookup=$(nslookup "$nume_host" 2>/dev/null | grep -A 1 'Name:' | grep 'Address:' | awk '{print $2}' | tail -n 1)
-        if [[ -n "$ip_nslookup" ]] && [[ "$ip_arg" != "$ip_nslookup" ]]; then
-            echo "Bogus IP for $nume_host in /etc/hosts! Config: $ip_arg, Resolved: $ip_nslookup"
-        fi
